7b5fd4acde6155cde8269cbdbc1fcf43bb5c338a
Yago Lemes da Cruz <yago.cruz123@gmail.com>
2026-06-08 20:03:33 -0300

    feat: implementado fluxo de inclusao de lancamentos e correcoes visuais de CLS

--- financeiro/urls.py
+++ financeiro/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
 
     # Nova rota para a interface visual da Etapa 4 (CASHFLOW) 🎨
     path('lancamentos/', views.listar_lancamentos, name='listar_lancamentos'),
+    path('lancamentos/incluir/', views.incluir_lancamento, name='incluir_lancamento'),
 
     # Rotas Auxiliares que o template precisa reconhecer para os botões funcionarem:
-    path('incluir/', views.listar_lancamentos, name='incluir_lancamento'), # Temporário apontando para a listagem
+    #path('incluir/', views.listar_lancamentos, name='incluir_lancamento'), # Temporário apontando para a listagem
     path('alterar/<int:pk>/', views.listar_lancamentos, name='alterar_lancamento'), # Temporário
     path('excluir/<int:pk>/', views.listar_lancamentos, name='excluir_lancamento'), # Temporário
     path('parcelas/<int:pk>/', views.listar_lancamentos, name='gerar_parcelas'), # Temporário
